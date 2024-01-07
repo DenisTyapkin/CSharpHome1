1c49d31c0b0670b8c5f9fab1b9a0cbb667d45788
Denis <denis.tuapkin@gmail.com>
2024-01-07 23:25:26 +0500

    Решение четвертой задачи, помогал Мураджон)

---
+++ Home1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int number2 = number;
+while (number2 > 9)
+{
+    number2 /= 10;
+    i *= 10;
+}
+while (number > 9)
+{
+    Console.Write($"{number / i},");
+    number %= i;
+    i /= 10;
+}
+Console.Write(number);
