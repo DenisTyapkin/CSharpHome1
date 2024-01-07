1ea063323253a79084c6c23c3814d5830ea466e8
Denis <denis.tuapkin@gmail.com>
2024-01-07 21:28:37 +0500

    Первая задача

---
+++ Home1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, 
+// принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+    Console.Write($"Число {number} кратно 7 и 23");
+}
+else
+{
+    Console.Write($"Число {number} не подходит");
+}
